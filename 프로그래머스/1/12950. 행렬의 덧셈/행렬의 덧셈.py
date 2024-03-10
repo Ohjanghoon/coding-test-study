3e9f16b6fcc475240213c6a6a25d6bb914ca60dc
Ohjanghoon <100773684+Ohjanghoon@users.noreply.github.com>
2024-03-10 14:50:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 37.83 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,11 +1,20 @@
+import numpy as np
+
 def solution(arr1, arr2):
-    answer = []
     
-    for idx_1, row in enumerate(arr1):
-        tmp = []
-        for idx_2, val in enumerate(row):
-            tmp.append(val + arr2[idx_1][idx_2])
-        answer.append(tmp)
-        
+    #1. 파이썬 일반적인 풀이 사용
+#     answer = []
+    
+#     for idx_1, row in enumerate(arr1):
+#         tmp = []
+#         for idx_2, val in enumerate(row):
+#             tmp.append(val + arr2[idx_1][idx_2])
+#         answer.append(tmp)
+    
+    #2. numpy 활용
+    answer = [[]]
+    
+    # tolist()를 활용하여 list 타입으로 변환
+    answer = (np.array(arr1) + np.array(arr2)).tolist()
             
     return answer
