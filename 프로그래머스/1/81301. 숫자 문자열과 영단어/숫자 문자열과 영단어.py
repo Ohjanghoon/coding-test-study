c6b70221f0a115aefa2ae63a52be242e67285c52
Ohjanghoon <100773684+Ohjanghoon@users.noreply.github.com>
2024-03-14 22:04:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = 0
+    matching_num = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    }
+    
+    if s.isdigit():
+        return int(s)
+    else:
+        for key, value in matching_num.items():
+            if key in s:
+                s = s.replace(key, str(value))
+        return int(s)
+                
