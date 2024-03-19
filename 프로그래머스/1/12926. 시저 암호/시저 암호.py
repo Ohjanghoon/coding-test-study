1b594fcc39947d6ce33e46affd3969e4d0d88e8c
Ohjanghoon <100773684+Ohjanghoon@users.noreply.github.com>
2024-03-19 22:04:21 +0900

    [level 1] Title: 시저 암호, Time: 2.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,14 +1,28 @@
 def solution(s, n):
     answer = ''
+    upper_alpha = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    lower_alpha = [chr(i) for i in range(ord('a'), ord('z') + 1)]
     
-    
+    # 2. list index 활용
     for alpha in s:
         if alpha == ' ':
             answer += ' '
         else:
             if alpha.isupper():
-                answer += chr((ord(alpha) - ord('A') + n) % 26 + ord('A'))
+                idx = (upper_alpha.index(alpha) + n) % 26
+                answer += upper_alpha[idx]
             else:
-                answer += chr((ord(alpha) - ord('a') + n) % 26 + ord('a'))
+                idx = (lower_alpha.index(alpha) + n) % 26
+                answer += lower_alpha[idx]
+    
+    # 1. ord 활용 풀이
+    # for alpha in s:
+    #     if alpha == ' ':
+    #         answer += ' '
+    #     else:
+    #         if alpha.isupper():
+    #             answer += chr((ord(alpha) - ord('A') + n) % 26 + ord('A'))
+    #         else:
+    #             answer += chr((ord(alpha) - ord('a') + n) % 26 + ord('a'))
                 
     return answer
